c4b10f6fce1db96d746e42b33dfc70d7afdd31c6
Martin <martin.stankard@hotmail.com>
2018-06-30 05:01:46 -0400

    getting posts and redux dev tool is functioning

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import Posts from './components/Posts';
 import Postform from './components/Postform';
+import {Provider} from 'react-redux';
+import store from './store';
+
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Postform/>
-        <hr/>
-        <Posts/>
-      </div>
+      <Provider store = {store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">An extremely verbose Redux implementation</h1>
+          </header>
+          <Postform/>
+          <hr/>
+          <Posts/>
+        </div>
+      </Provider>
     );
   }
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {fetchPosts} from '../actions/postActions';
 
 class Posts extends Component {
 
@@ -12,14 +15,12 @@ class Posts extends Component {
 
 componentWillMount(){
     console.log('1.Posts.componentWillMount');
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(json => this.setState({posts:json}));
+    this.props.fetchPosts();
 }
   
    render() {
        console.log("2.Posts.render");
-       const postItems = this.state.posts.map(post =>(
+       const postItems = this.props.posts.map(post =>(
         <div key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>    
@@ -60,5 +61,13 @@ componentWillUpdate(){
 
 }
 
+Posts.propTypes = {
+    fetchPosts:PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired 
+}
 
-export default Posts;
+const mapStateToProps = state => ({
+    posts:state.posts.items
+});
+
+export default connect(mapStateToProps, {fetchPosts})(Posts);
